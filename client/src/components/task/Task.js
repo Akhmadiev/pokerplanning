55dea38bd7f567fbbd19a2484373a9bea891465b
Марат Ахмадиев <m.akhmadiev@bars.group>
2022-09-01 01:33:14 +0300

    qwe

--- client/src/components/task/Task.js
+++ client/src/components/task/Task.js
@@ -17,7 +17,7 @@ const Task = (props) => {
     const createTask = useMutation(async () => { return await QueryService.createTask(fromPage.replace('/', ''), newTask) }, {
         onSuccess: (response) => {
             setData(response.data);
-            setNewTask({ value: "", vote: 0, votes: Array(0) });
+            setNewTask({ value: "", vote: 0, votes: Array(0), description: "" });
             socket.emit("refetch", data.id);
         }
     });
@@ -38,59 +38,63 @@ const Task = (props) => {
     
     const onChange = (evt) => {
         var value = evt.target.value;
-        setNewTask({ value: value, vote: 0, votes: Array(0) });
+        setNewTask({ value: value, vote: 0, votes: Array(0), description: newTask.description });
     }
 
-    // const onVoteChange = (evt) => {
-    //     var value = evt.target.value;
-    //     QueryService.changeTaskVote(fromPage.replace('/', ''), props.voteTaskId, value);
-    // }
+    const onDescriptionChange = (evt) => {
+        var value = evt.target.value;
+        newTask.description = value;
+        setNewTask(newTask);
+    }
 
     return (
         <div>
             {props.created ?
                 (
-                    <div className="input-group mb-3" style={{ width: "100%" }}>
-                        <input
-                            type="number"
-                            className="form-control"
-                            placeholder="est"
-                            aria-label="JIRA"
-                            aria-describedby="basic-addon2"
-                            value={props.task.vote}
-                            disabled
-                            // onChange={(evt) => onVoteChange(evt)}
-                            style={{ maxWidth: "7%" }} />
-                        <a style={{ width: "68%", textAlign: "center", border: "thick double #32a1ce", backgroundColor: props.voteTaskId === task.id ? "#90EE90" : "" }} href={task?.value}>{task?.value}</a>
-                        <button
-                            disabled={isLoading}
-                            onClick={() => voteTask.mutate(props.task.id)}
-                            style={{ width: "10%", backgroundColor: "#90EE90" }}
-                            className="btn btn-outline-secondary"
-                            type="button">
-                            Vote
-                        </button>
-                        <button
-                            disabled={isLoading}
-                            onClick={() => deleteTask.mutate(props.task.id)}
-                            style={{ width: "15%", backgroundColor: "#FFCCCB" }}
-                            className="btn btn-outline-secondary"
-                            type="button">
-                            Delete
-                        </button>
+                    <div className="task">
+                        <textarea class="form-control" id="exampleFormControlTextarea1" value={task?.description} placeholder="Description" disabled aria-label="Description" rows="2" />
+                        <div className="input-group mb-3" style={{ width: "100%" }}>
+                            <input
+                                type="number"
+                                className="form-control"
+                                aria-label="JIRA"
+                                disabled
+                                value={task?.vote}
+                                aria-describedby="basic-addon2"
+                                // onChange={(evt) => voteChange.mutate(props.task.id, evt.target.value)}
+                                style={{ maxWidth: "9%" }} />
+                            <a style={{ width: "66%", textAlign: "center", border: "thick double #32a1ce", backgroundColor: props.voteTaskId === task.id ? "#90EE90" : "" }} href={task?.value}>{task?.value}</a>
+                            <button
+                                disabled={isLoading}
+                                onClick={() => voteTask.mutate(props.task.id)}
+                                style={{ width: "10%", backgroundColor: "#90EE90" }}
+                                className="btn btn-outline-secondary"
+                                type="button">
+                                Vote
+                            </button>
+                            <button
+                                disabled={isLoading}
+                                onClick={() => deleteTask.mutate(props.task.id)}
+                                style={{ width: "15%", backgroundColor: "#FFCCCB" }}
+                                className="btn btn-outline-secondary"
+                                type="button">
+                                Delete
+                            </button>
+                        </div>
                     </div>
                 ) :
                 (
                     <div>
+                        <textarea onChange={(evt) => onDescriptionChange(evt)} class="form-control" id="exampleFormControlTextarea1" value={newTask?.description} placeholder="Description" aria-label="Description" rows="2" />
                         <div className="input-group mb-3">
                             <input
                                 disabled={isLoading}
                                 type="text"
                                 className="form-control"
-                                placeholder="JIRA"
-                                aria-label="JIRA"
+                                placeholder="JIRA link"
+                                aria-label="JIRA link"
                                 aria-describedby="basic-addon2"
-                                value={newTask.value}
+                                value={newTask?.value}
                                 onChange={(evt) => onChange(evt)}
                             />
                             <div className="input-group-append">
--- client/src/components/task/Tasks.js
+++ client/src/components/task/Tasks.js
@@ -21,7 +21,7 @@ const Tasks = () => {
   return (
     <div className="right">
       <div className="tasks-total">
-        <div style={{ position: "fixed", fontStyle: "italic" }}><h1>Total story points:<span className="badge badge-secondary" style={{ color: "black", right: 0 }}>{totalVote}</span></h1></div>
+        {/* <div style={{ position: "fixed", fontStyle: "italic" }}><h1>Total story points:<span className="badge badge-secondary" style={{ color: "black", right: 0 }}>{totalVote}</span></h1></div> */}
         <div className="tasks">
           {rows}
           <Task />
